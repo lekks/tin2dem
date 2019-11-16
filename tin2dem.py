56b7f1136d0d42aeb9b4e047b96c156dc0fe7813
Alexey Dolinenko <lekks@yandex.ru>
2019-11-16 10:43:57 +0300

    manage debug information

--- tin2dem.py
+++ tin2dem.py
@@ -3,56 +3,61 @@
 import argparse
 import logging
 import sys
+import time
+
+from tqdm import tqdm
 
 from dem_file import GeoTiff
 from dem_geo import DemInfo, split_dem
 from render import Render
 from tinn_read import Surface
-from tqdm import tqdm
 
-log = logging.getLogger("tinn to dem")
+log = logging.getLogger("tin to dem")
 
-CHUNK_WIDTH = 256
-CHUNK_HEIGHT = 256
-
+CHUNK_DEFAULT = 256
 NO_DATA_VALUE = -32767
 
-def tin2dem(tinn_file, tiff_file, pix_size, epsg):
+
+def tin2dem(tinn_file, tiff_file, pix_size, epsg, margins, chunk_width, chunk_height):
+    start = time.time()
     if epsg is None:
         log.warn("NO EPSG GIVEN")
-    log.info("Let`s read tinn {}".format(tinn_file))
+    print ("Reading tin file {} ...".format(tinn_file))
     surface = Surface()
     surface.read_tin(tinn_file)
-    log.info("{} points".format(len(surface.vertices)))
-    log.info("{} faces".format(len(surface.faces)))
-    log.info("Min corner is {}".format(surface.min_vertex))
-    log.info("Max corner is {}".format(surface.max_vertex))
+    print ("Found {} points and {} faces".format(len(surface.vertices), len(surface.faces)))
+    print ("Min corner is {}".format(surface.min_vertex))
+    print ("Max corner is {}".format(surface.max_vertex))
     shift = surface.shift_origin()
-    render = Render(surface, NO_DATA_VALUE)
-    dem = DemInfo.from_envelope(*surface.get_envelope(), pix_size=pix_size, margins=0)
-    print (dem.gt)
-    print ("Shift {}".format(shift))
+    dem = DemInfo.from_envelope(*surface.get_envelope(), pix_size=pix_size, margins=margins)
     tiff = GeoTiff(tiff_file, dem, NO_DATA_VALUE, shift, epsg)
 
-    chunks = list(split_dem(dem, CHUNK_WIDTH, CHUNK_WIDTH))
+    print ("Preprocessing...")
+    render = Render(surface, NO_DATA_VALUE)
+    print ("Rendering...")
+    chunks = list(split_dem(dem, chunk_width, chunk_height))
     for chunk in tqdm(chunks):
         filter_buf = render.filter_bounds(chunk)
         result, debug = render.render_dem(chunk, filter_buf)
         tiff.write_chunk(chunk, result)
+    end = time.time()
+    print ("Done in {0:.2f} seconds".format(end - start))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("tinn")
+    parser.add_argument("tin")
     parser.add_argument("tiff")
     parser.add_argument("--pixel", type=float, default=1.0)
     parser.add_argument("--epsg", type=int, default=None)
+    parser.add_argument("--chunk", type=int, default=CHUNK_DEFAULT)
+    parser.add_argument("--margins", type=float, default=None)
     args = parser.parse_args()
-    tin2dem(args.tinn, args.tiff, args.pixel, args.epsg)
+    tin2dem(args.tin, args.tiff, args.pixel, args.epsg, args.margins, args.chunk, args.chunk)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout,
+    logging.basicConfig(level=logging.WARN, stream=sys.stdout,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     main()
